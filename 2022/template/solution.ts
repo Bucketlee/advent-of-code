4a0e9420e2bc582af44d04124db773a70b410643
chanyoung <chanyoung.cedu@gmail.com>
2022-12-05 20:25:37 +0900

    feat: change template

--- 2022/template/solution.ts
+++ 2022/template/solution.ts
@@ -1,2 +1,13 @@
-function solution() {
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filePath = path.join(__dirname, '/input.txt');
+const text = fs.readFileSync(filePath);
+const textByLine = text.toString().split('\n');
+
+console.log(textByLine)
+
+function solution(input: string[]) {
 }
+
+console.log(solution(textByLine));
